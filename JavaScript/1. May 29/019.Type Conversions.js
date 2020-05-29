2b9783e3d03e86733ca6d9f0f5655c32510d079d
salman-ansari-vst-au4 <salmanahmed1497@gmail.com>
2020-05-29 12:19:32 +0530

    Day 1 (May 29) v0.1

---
+++ JavaScript/1. May 29/019.Type Conversions.js
@@ -0,0 +1,15 @@
+let x = 2 + '2'; // 22
+
+// javaScript take easy path so instead of converting string to num it convert num to string
+
+x = 2 + 2 + '2'; //42
+
+// go from left to right so 2+2 = 4 + '2' = '42'
+
+x = 2 + true; // 3;
+
+// because true = 1 & false = 0
+
+x = true + '2'; // 'true2'
+
+// its easy to convert boolean to string than string to boolean
